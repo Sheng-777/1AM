61dff049586d9a856bb9b888bb7e62021a06393b
Sheng-777 <hanshengliu2024@gmail.com>
2023-12-01 18:18:49 -0500

    Create dynamic link for posts

---
+++ pages/posts/[post].tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function TestPost(){
+    const router = useRouter();
+    const postID = router.query.post;
+
+    return(
+        <div>
+        <Link href="/posts"></Link>
+      <div>
+        <p className="font-bold text-4xl text-gray-800 text-center">Post {postID} Details Page</p>
+      </div>
+        </div>
+    )
+}
---
+++ pages/posts/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Posts(){
+  const postIDs = ["1", "2", "3"];
+  return (
+    <>
+      {postIDs.map((post, key) => (
+        <Link href="/posts/[post]" as={`posts/${post}`} key={key}>
+        </Link>
+      ))}
+    </>
+  );
+};
