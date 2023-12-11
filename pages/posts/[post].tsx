01f42ca28a369bd12d615ea80355d34a1fc7c8e9
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-12-11 16:45:51 -0500

    idk installed something by accident

--- pages/posts/[post].tsx
+++ pages/posts/[post].tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Post from "@/components/Post";
 
 export default function PostPage(){
     const router = useRouter();
     const postID = router.query.post;
 
     return(
-        <div>
-        <Link href="/posts"></Link>
-      <div>
-        <p className="font-bold text-4xl text-gray-800 text-center">Post {postID} Details Page</p>
-      </div>
+        <div className="h-full">
+          <Link href="/posts"></Link>
+          <div className="grid grid-cols-2 h-full">
+            <div>
+              <p className="font-bold text-4xl text-gray-800 text-center">Post {postID} Details Page</p>
+            </div>
+            <div className="bg-red-100">
+            </div>
+          </div>
         </div>
     )
 }
