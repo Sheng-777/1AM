c62efcab46b1299739a897fbe3bc2b1b4d961d47
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-12-18 16:51:19 -0500

    warnings preventing deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import ScrollBar from '@/components/ScrollBar'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
