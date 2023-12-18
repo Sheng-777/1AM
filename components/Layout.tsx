365f4f52cce6e4b80d8d6e15b3ab8abec862e019
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-12-18 16:59:22 -0500

    idk

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,10 +6,6 @@ interface LayoutProps {
   }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  // For Dark Mode
-  
-
-
     return (
       <div className="flex">
         {/* Render SideBar */}
