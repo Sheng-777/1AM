2fa16f31a2af5970aa05755c46909a00b36ee448
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-12-18 21:03:20 -0500

    fixing imports

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, useEffect, useState } from "react";
-import SideNavBar from "./sideNavBar";
+import SideNavBar from "./SideNavBar";
 
 interface LayoutProps {
     children: ReactNode;
