f29e104f5d6f4e2d0eabbd6928fa464323805282
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-01-09 14:22:57 -0500

    random css stuff

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,30 @@
 import { ReactNode, useEffect, useState } from "react";
 import SideNavBar from "./SideNavBar";
+import { FloatButton } from 'antd';
+import { HiOutlinePlus } from "react-icons/hi2";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
     children: ReactNode;
   }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+
+  const goToNewPost = () => {
+    router.push('newPost')
+  }
+
     return (
       <div className="flex">
         {/* Render SideBar */}
+          <FloatButton
+            onClick={goToNewPost}
+            className="w-12 h-12"
+            style={{ right: 40, bottom: 40}}
+            icon={<HiOutlinePlus/>}
+            tooltip="New Post"
+          />
         <div>
           <SideNavBar/>
         </div>
--- pages/boards/[board]/[post].tsx
+++ pages/boards/[board]/[post].tsx
@@ -14,7 +14,7 @@ export default function PostPage(){
     return(
       <div className="h-full">
         <Link href={`/posts`}></Link>
-        <div className="grid grid-cols-2 h-full">
+        <div className="grid grid-cols-2 h-full w-full">
           <div className="overflow-scroll no-scrollbar">
             <p className="font-bold text-4xl text-gray-800 text-center">{currentPost?.title}</p>
             <div className="columns-3 m-3 gap-3">
@@ -29,7 +29,7 @@ export default function PostPage(){
                         
                       }>
                       <div className="w-auto h-auto mb-3 rounded-lg overflow-hidden shrink-0 shadow-md bg-white dark:bg-gray-600 hover:shadow-gray-500 dark:hover:shadow-black transition-shadow duration-300 ease-in-out">
-                          <Post post={post} className="w-auto h-auto object-cover" ></Post>
+                          <Post post={post} width={600} height={600} className="w-auto h-auto object-cover" ></Post>
                       </div>
                       </Link>
                     </div>
@@ -38,9 +38,9 @@ export default function PostPage(){
               )}
               </div>
           </div>
-          <div className="bg-red-100">
+          <div className="bg-red-100 w-full h-full">
             {postID ? (
-              <Post post={currentPost} className="w-64 h-64 object-cover" />
+              <Post post={currentPost} className="w-full h-full bg-blue-200" />
             ) : null}
           </div>
         </div>
