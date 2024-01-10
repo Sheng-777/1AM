7d647c5d065f5653352c67e076d3c37ce4aa4a7c
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-01-09 19:59:39 -0500

    float button...

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,8 @@
 import { ReactNode, useEffect, useState } from "react";
 import SideNavBar from "./SideNavBar";
-import { FloatButton } from 'antd';
-import { HiOutlinePlus } from "react-icons/hi2";
 import { useRouter } from "next/router";
+import { HiOutlinePlus } from "react-icons/hi2";
+import Link from "next/link";
 
 interface LayoutProps {
     children: ReactNode;
@@ -18,16 +18,15 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
       <div className="flex">
         {/* Render SideBar */}
-          <FloatButton
-            onClick={goToNewPost}
-            className="w-12 h-12"
-            style={{ right: 40, bottom: 40}}
-            icon={<HiOutlinePlus/>}
-            tooltip="New Post"
-          />
         <div>
           <SideNavBar/>
         </div>
+        <Link href="/newPost" className="tooltip flex place-content-center w-14 h-14 rounded-full absolute right-10 bottom-10 shadow-lg bg-white transition-colours duration-300 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+            <HiOutlinePlus className="w-8 h-8 place-self-center text-gray-600"/>
+            <div className="tooltiptext w-24">
+              New Post
+            </div>
+        </Link>
         {/* Render Page */}
         <div className="w-[calc(100vw_-_256px)] h-screen overflow-hidden">
           {children}
