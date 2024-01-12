ea05bc388190ca31cbfff69747e2a3f3a888f5e5
Sheng-777 <hanshengliu2024@gmail.com>
2024-01-12 12:00:45 -0500

    Fix Signup

--- components/Forms/SignupForm.tsx
+++ components/Forms/SignupForm.tsx
@@ -51,7 +51,7 @@ const SignUpForm = () => {
     if (isValid){
       try{
         setLoading(true)
-        const apiRes = await axios.post("https://www.rouge-co.com/api/services/auth/signup", formData)
+        const apiRes = await axios.post("https://www.rouge-co.com/api/auth/signup", formData)
         //console.log(apiRes)
         if (apiRes?.status === 200){
           // save data in session
