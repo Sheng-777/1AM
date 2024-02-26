5d4cb780994e41fe117e3a34f084e448520eb549
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-02-26 17:26:46 -0500

    localhost:3000 to rouge-co.com

--- pages/newPost.tsx
+++ pages/newPost.tsx
@@ -124,7 +124,7 @@ async function handleOnSubmit() {
     if (isValid){
       try{
         setLoading(true)
-        const apiRes = await axios.post("http://localhost:3000/api/services/createPost", formData)
+        const apiRes = await axios.post("https://www.rouge-co.com/api/services/createPost", formData)
         if (apiRes?.status === 200){
           // save data in session
           //console.log(apiRes)
